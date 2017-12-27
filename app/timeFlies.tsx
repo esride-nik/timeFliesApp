568e2bc081bc7416adf26b8ade6595cc5cfe50b3
Nik <n.koehn@esri.de>
2017-12-28 00:03:44 +0100

    timeFlies widget: Pause/Resume

--- app/timeFlies.tsx
+++ app/timeFlies.tsx
@@ -9,6 +9,8 @@ import Point = require("esri/geometry/Point");
 import SpatialReference = require("esri/geometry/SpatialReference");
 import Accessor = require("esri/core/Accessor");
 import Deferred = require("dojo/Deferred");
+import dom = require("dojo/dom");
+import domClass = require("dojo/dom-class");
 import { subclass, declared, property } from "esri/core/accessorSupport/decorators";
 import { renderable, tsx } from "esri/widgets/support/widget";
 
@@ -27,6 +29,21 @@ interface TimeFliesParams {
 
 @subclass("esride.widgets.TimeFlies")
 class TimeFlies extends declared(Widget) {
+
+    @property()
+    _animationPlaying: boolean;
+
+    @property()
+    _currentFeature: number;
+
+    @property()
+    _features: Graphic[];
+
+    @property()
+    _goToTarget: any;
+
+    @property()
+    _goToOptions: any;
 
     @property()
     _flightLayer: FeatureLayer;
@@ -95,6 +112,7 @@ class TimeFlies extends declared(Widget) {
         this._sceneView = params.sceneView;
         this._zoomInLevel = params.zoomInLevel;
         this._zoomOutLevel = params.zoomOutLevel;
+        this._animationPlaying = true;
         
         var query = new Query();
         query.where = "1=1";
@@ -105,7 +123,8 @@ class TimeFlies extends declared(Widget) {
         
         this._flightLayer.queryFeatures(query).then((results: FeatureSet) => {
           console.log("TimeFlies Result", results.features);
-          this.iterateThroughFeaturesSynchronously(results.features, 0);
+          this._features = results.features;
+          this.iterateThroughFeaturesSynchronously(0);
 
           // ToDo: Create animation for alle features
           // ToDo: Open popups automatically
@@ -113,10 +132,12 @@ class TimeFlies extends declared(Widget) {
           // ToDo: create route to all features, put into FL with ID corresponding to point and highlight route feature on each animation
         });
     }
-
-    iterateThroughFeaturesSynchronously(features: Graphic[], i: number) {
+    
+    iterateThroughFeaturesSynchronously(i: number) {
+        var features: Graphic[] = this._features;
+        this._currentFeature = i;
         // don't iterate via features.map(), because this executes asynchonously and doesn't wait for the animation to finish
-        if (features.length>i) {
+        if (features.length>i && this._animationPlaying) {
             this.fid = (features[i] as Graphic).attributes.FID;
             
             // Feature selection is not yet supported in JS4, so we can't select a feature on the layer and just display the popup. We need to care for the display ourselves instead.
@@ -134,7 +155,7 @@ class TimeFlies extends declared(Widget) {
             
             this.zoomAndCenterOnFeature(features[i]).then((evt: any) => {
                     i++;
-                    this.iterateThroughFeaturesSynchronously(features, i);
+                    this.iterateThroughFeaturesSynchronously(i);
                 }
             );
         }
@@ -155,15 +176,41 @@ class TimeFlies extends declared(Widget) {
             longitude = (feature.geometry as Point).longitude;
         }
 
-        return this._sceneView.goTo({
+        this._goToTarget = {
             zoom: this._zoomInLevel,
             center: [longitude, latitute],
             tilt: 75
-        }, {
+        }
+        this._goToOptions = {
             animate: true,
             duration: 4000,
             easing: "in-out-cubic"
-        });
+        }
+
+        return this._sceneView.goTo(this._goToTarget, this._goToOptions);
+    }
+    
+    createMarkup() {
+        return {__html: '<a target="_blank" href="http://www.colognenightofmusic.de">cologne night of music</a>'};
+    }
+
+    pauseFlight() {
+        console.log("pause", this);
+        domClass.add(dom.byId("btnPause"), "is-active");
+        domClass.remove(dom.byId("btnResume"), "is-active");
+        this._animationPlaying = false;
+    }
+
+    resumeFlight() {
+        console.log("resume", this);
+        domClass.add(dom.byId("btnResume"), "is-active");
+        domClass.remove(dom.byId("btnPause"), "is-active");
+        this._animationPlaying = true;
+        this.iterateThroughFeaturesSynchronously(this._currentFeature);
+    }
+
+    afterCreate() {
+        console.log("afterCreate", this);
     }
 
     render() {
@@ -172,14 +219,23 @@ class TimeFlies extends declared(Widget) {
             [CSS.esrideTimeFlies]: true
         };
         return (
-            <div bind={this}
+            <div bind={this} afterCreate={this.afterCreate}
                 class={CSS.base}
                 classes={classes}>
+                <div
+                class="content"
+                dangerouslySetInnerHTML={{ __html: '<a target="_blank" href="http://www.colognenightofmusic.de">cologne night of music</a>'}}
+              />
+              <p>
+                <button bind={this} onclick={this.resumeFlight} class="btn is-active" id="btnResume"><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32" class="svg-icon"><path d="M6 0l22 16.002L6 32V0z"/></svg></button>
+                <button bind={this}  onclick={this.pauseFlight} class="btn" id="btnPause"><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32" class="svg-icon"><path d="M26 4v24h-6V4h6zM6 28h6V4H6v24z"/></svg></button>
+              </p>
                 Gig No.: <b>{this.nr}<br />
                 {this.wochentag}, {this.date}<br />
                 {this.location}<br />
                 Ort: {this.ort}</b><br />
                 Infos: {this.infos}<br />
+                Infos: <div dangerouslySetInnerHTML={this.createMarkup()} /><br />
                 PLZ: {this.plz}<br />
                 Stagetime: {this.stagetime}<br />
                 Tagebuch: {this.tagebuch}<br />
